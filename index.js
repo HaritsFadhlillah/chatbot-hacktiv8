281477add7bade8e21e7ea89cb124eaad542c234
HaritsFadhlillah <harits.fadhlillah123@gmail.com>
2026-07-04 15:52:53 +0700

    Penambahan Generate Image,Audio,Dokumen

--- index.js
+++ index.js
@@ -32,4 +32,29 @@ app.post('/generate-text', async (req, res) => {
         res.status(500).json({ error: "An error occurred while generating text." });
     }
 });
+app.post('/generate-from-document', upload.single('file'), async (req, res) =>{
+    try{
+        const { prompt } = req.body;
+        const fileBase64 = req.file.buffer.toString('base64');
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                {
+                    text: prompt,
+                    type: 'text',
+                },
+                {
+                    inlineData:{
+                        data:fileBase64,
+                        mimeType:req.file.mimetype,
+                    },
+                },
+            ],
+        });
+    res.status(200).json({result:response.text});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ error: "An error occurred while generating text from document." });
+    }
+});
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
